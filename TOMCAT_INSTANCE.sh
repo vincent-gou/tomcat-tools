234c4551d08a0d6f3df428713d80fa68d3cce293
Vincent Gourrierec <46958513+vincent-gou@users.noreply.github.com>
2019-11-20 18:23:13 +0100

    Add Jmx4perl checks

--- TOMCAT_INSTANCE.sh
+++ TOMCAT_INSTANCE.sh
@@ -2,10 +2,7 @@
 #set -x
 SOURCE_PATH=$(pwd)
 SCRIPT_PATH=$(dirname $0)
-#echo $SOURCE_PATH
-#echo $SCRIPT_PATH
 CONFIG_PATH=$SCRIPT_PATH/../config/local.sh
-#echo $CONFIG_PATH
 
 . $CONFIG_PATH
 
@@ -26,29 +23,38 @@ if [ -z  $1 ]
       echo -e "\e[43m";;
     BOLD) 
       echo -e "\e[1m";;
+    UNDERL)
+      echo -e "\e[4m";;
   esac
 fi
 }
 
 Instance_Info() {
-echo -e "$(Color WHITE)INSTANCE:\t\t$(Color)$(Instance)"
+echo -e "$(Color WHITE)Legende:$(Color)\t\tJMX4PERL: $(Color WHITE)$(Color BOLD)$(Color UNDERL)VALEUR$(Color)"
+echo -e "$(Color WHITE)INSTANCE:\t\t$(Color)$(Color BOLD)$(Instance)$(Color) "
+echo -e "$(Color WHITE)Demarree depuis:\t$(Color)$(Color BOLD)$(Start_time)$(Color) "
 echo -e "$(Color WHITE)CATALINA_HOME:\t\t$(Color)$CATALINA_HOME"
 echo -e "$(Color WHITE)CATALINA_BASE:\t\t$(Color)$CATALINA_BASE"
 echo -e "$(Color WHITE)JAVA_HOME:\t\t$(Color)$JAVA_HOME"
 echo -e "$(Color WHITE)UNIT SYSTEMD:$(Color)\t\tinstalled: $(Systemd_enable)\tStatut: $(Systemd_status)"
+echo -e "$(Color WHITE)Controles:$(Color)\t\tJDK: $(Check_jdk)\tJstat: $(Check_jstat)\tJmx4perl: $(Check_jmx4perl)"
 echo -e "$(Color WHITE)Infos Techniques:$(Color)"
-echo -e "\t$(Color BOLD)Port$(Color)\t$(Color BOLD)Threads$(Color)\t$(Color BOLD)Accept $(Color)\t$(Color BOLD)Compr$(Color)\t$(Color BOLD)Connector$(Color)"
-echo -e "\t$(Color BOLD)$(Color)\t$(Color BOLD)Max$(Color)\t$(Color BOLD)Count$(Color)\t\t$(Color BOLD)Type$(Color)"
-echo -e "\t$(Color BOLD)----$(Color)\t$(Color BOLD)------$(Color)\t$(Color BOLD)----$(Color)\t$(Color BOLD)----$(Color)\t$(Color BOLD)----$(Color)"
-echo -e "Config:\t$(Color BOLD)$(Port_http)$(Color)\t$(Color BOLD)$(Max_threads)$(Color)\t$(Color BOLD)$(Accept_count)$(Color)\t$(Color BOLD)$(Compression)$(Color)\t$(Color BOLD)$(Connector_type)$(Color)"
+echo -e "\t$(Color BOLD)Port$(Color)\t$(Color BOLD)Thread$(Color)\t$(Color BOLD)Accept $(Color)\t$(Color BOLD)Compr$(Color)\t$(Color BOLD)Connect$(Color)\t$(Color BOLD)XMX$(Color)\t$(Color BOLD)XMS$(Color)"
+echo -e "\t$(Color BOLD)$(Color)\t$(Color BOLD)PoolMax$(Color)\t$(Color BOLD)Count$(Color)\t\t$(Color BOLD)Type$(Color)\t$(Color BOLD)Mo$(Color)\t$(Color BOLD)Mo$(Color)"
+echo -e "\t$(Color BOLD)----$(Color)\t$(Color BOLD)------$(Color)\t$(Color BOLD)----$(Color)\t$(Color BOLD)----$(Color)\t$(Color BOLD)----$(Color)\t$(Color BOLD)----$(Color)\t$(Color BOLD)----$(Color)"
+echo -e "Config:\t$(Color BOLD)$(Port_http_check)$(Color)\t$(Color BOLD)$(Max_threads)$(Color)\t$(Color BOLD)$(Accept_count)$(Color)\t$(Color BOLD)$(Compression)$(Color)\t$(Color BOLD)$(Connector_type)$(Color)\t$(Color BOLD)$(Jvm_xmx_config)$(Color)\t$(Color BOLD)$(Jvm_xms_config)$(Color)"
 
 if  [  -n "$(Pid)" ]
   then
-    echo -e "=====================================================" 
-    echo -e "\t$(Color BOLD)Threads$(Color)\t"
-    echo -e "\t$(Color BOLD)-------$(Color)\t"
-    echo -e "$(Color BOLD)Actuel:$(Color)\t$(Color BOLD)$(Current_threads)/$(Max_threads)\t  "
+    echo -e "============================================================================================================" 
+    echo -e "\t$(Color BOLD)Threads\tHeap\tJVM \tRam   \tSession\tRequest\tProcess\tThread \tAvg   $(Color)"
+    echo -e "\t$(Color BOLD)OS     \tSize\tCPU \tMemory\tActive \tCount  \tTimeAvg\tPool   \tError $(Color)"
+    echo -e "\t$(Color BOLD)       \t    \t    \t      \tCount  \t       \t       \tCur/Mx \tRate  $(Color)"
+    echo -e "\t$(Color BOLD)-------\t----\t ---\t------\t-------\t-------\t-------\t-------\t------$(Color)"
+    echo -e "$(Color BOLD)Actuel:\t$(Current_threads)\t$(Jvm_heap_memory)\t$(Jvm_cpu_use)\t$(Jvm_ram_memory)\t$(Session_active_count)\t$(Request_count)\t$(Avg_process_time)\t$(Thread_pool_buzyness)\t$(Error_average)$(Color)"
+    echo -e "$(Color BOLD)Max:\t-----\t$(Jvm_max_heap_memory)\t-----\t-----\t$(Session_max_active_count)\t-------\t-------\t-------\t-------$(Color)"
 fi
+echo ""
 status 1 
 }
 
@@ -57,20 +63,35 @@ INSTANCE=$(echo $CATALINA_BASE |  awk -F/ '{print $(NF-2)}' )
 echo $INSTANCE
 }
 
-Port_http() {
-  PORT_HTTP=$(cat $CATALINA_BASE/conf/server.xml | grep "Connector port=" | awk '{print $2}'| sed -e "s/=/ /g"| awk '{print $2}' | sed -e "s/\"//g")
-#$(Pid)
+Start_time() {
 if  [  -n "$(Pid)" ]
   then
-    STATE_PORT=$(netstat -anp 2>/dev/null | grep $(Pid) | grep LISTEN &>/dev/null)  
-  if [ $? -eq 0 ]  
-    then
-    echo $(Color GREEN)$PORT_HTTP$(Color)
-  fi
-  else 
-    echo $(Color RED)$PORT_HTTP$(Color)
-fi  
+    START_TIME=$(ps -eo pid,cmd,lstart | grep $(Pid) | grep -v grep | awk '{print $5" "$4" "$7" a: "$6}')
+    echo $START_TIME
+  else
+    echo $(Color RED)N/A$(Color) 
+fi
 }
+
+Port_http() {
+PORT_HTTP=$(cat $CATALINA_BASE/conf/server.xml | grep "Connector port=" | awk '{print $2}'| sed -e "s/=/ /g"| awk '{print $2}' | sed -e "s/\"//g")
+echo $PORT_HTTP
+}
+
+Port_http_check() {
+#PORT_HTTP=$(cat $CATALINA_BASE/conf/server.xml | grep "Connector port=" | awk '{print $2}'| sed -e "s/=/ /g"| awk '{print $2}' | sed -e "s/\"//g")
+if  [  -n "$(Pid)" ]
+  then
+    STATE_PORT=$(netstat -anp 2>/dev/null | grep $(Pid) | grep LISTEN &>/dev/null)
+  if [ $? -eq 0 ]
+    then
+    echo $(Color GREEN)$(Port_http)$(Color)
+  fi
+  else
+    echo $(Color RED)$(Port_http)$(Color)
+fi
+}
+
 
 Compression() {
   COMPRESSION=$(cat $CATALINA_BASE/conf/server.xml | grep "compression=" | sed -e "s/=/ /g"| awk '{print $2}' | sed -e "s/\"//g")
@@ -95,10 +116,10 @@ echo $ACCEPT_COUNT
 Connector_type() {
 CONNECTOR_TYPE=$(cat $CATALINA_BASE/conf/server.xml | grep "protocol=" | awk '{print $3}'| sed -e "s/=/ /g"| awk '{print $2}' | sed -e "s/\"//g")
 if [ $CONNECTOR_TYPE == HTTP/1.1 ]
-  then echo $CONNECTOR_TYPE
+  then echo DEFAULT
   else 
     CONNECTOR_TYPE=$(cat $CATALINA_BASE/conf/server.xml | grep "protocol=" | awk '{print $3}'| sed -e "s/=/ /g"| awk '{print $2}' | sed -e "s/\"//g" | sed -e "s/\./ /g" | awk '{print $5}' | sed -e "s/Protocol//g" | sed -e "s/Http11//g")
-    echo ${CONNECTOR_TYPE^^}
+    echo $CONNECTOR_TYPE | tr '[:lower:]' '[:upper:]'
 fi
 }
 
@@ -115,7 +136,7 @@ if  [  -n "$(Pid)" ]
 	if [ $THREAD_USED_PERCENT -gt 100 ]		
 	  then 	
 	    WAITING_THREADS=$(echo "$CURRENT_THREADS - $(Max_threads)" |bc)	
-            echo -e $(Color RED)$CURRENT_THREADS waiting:$WAITING_THREADS$(Color) 
+            echo -e $(Color RED)$CURRENT_THREADS w:$WAITING_THREADS$(Color) 
 	  else
 	    echo -e $(Color YELLOW)$CURRENT_THREADS$(Color)	
 	fi
@@ -124,6 +145,155 @@ fi
 
 }
 
+Check_jdk() {
+if [ ! -z $JDK_HOME -a -d $JDK_HOME ]
+  then echo "$(Color BOLD)$(Color GREEN)OK$(Color)"
+  else echo "$(Color BOLD)$(Color YELLOW)NON$(Color)"
+fi
+}
+
+Check_jstat() {
+if [ ! -z $JDK_HOME -a -x $JDK_HOME/bin/jstat ]
+  then echo -e "$(Color BOLD)$(Color GREEN)OK$(Color)"
+  else echo -e "$(Color BOLD)$(Color YELLOW)NON$(Color)"
+fi
+}
+
+Check_jmx4perl() {
+if [ -x /usr/bin/jmx4perl ]
+  then echo -e "$(Color BOLD)$(Color GREEN)OK$(Color)"
+  else echo -e "$(Color BOLD)$(Color YELLOW)NON$(Color)"
+fi
+}
+
+Jvm_heap_memory() { 
+if [[ $(Check_jmx4perl) == *OK* ]]
+  then 
+    HEAP_MEMORY=$((`/usr/bin/jmx4perl http://localhost:$(Port_http)/j4p --product tomcat read "java.lang:type=Memory" HeapMemoryUsage used` / 1024 / 1024 ))
+    echo $(Color WHITE)$(Color BOLD)$(Color UNDERL)$HEAP_MEMORY$(Color) Mo
+  else
+  if [[ $(Check_jstat) == *OK* ]]
+    then 
+      HEAP_MEMORY=$( ($JDK_HOME/bin/jstat -gc $(Pid) 2>/dev/null || echo "0 0 0 0 0 0 0 0 0") | tail -n 1 | awk '{split($0,a," "); sum=a[3]+a[4]+a[6]+a[8]; print sum/1024}' ) 2>/dev/null
+      HEAP_MEMORY=${HEAP_MEMORY%.*}
+      echo -e $(Color BOLD)$HEAP_MEMORY Mo$(Color)
+    else
+      echo -e $(Color BOLD)$(Color YELLOW)No JDK/JMX$(Color)
+  fi
+fi
+}
+
+
+Jvm_max_heap_memory() {
+if [[ $(Check_jmx4perl) == *OK* ]]
+  then
+    HEAP_MEMORY=$((`/usr/bin/jmx4perl http://localhost:$(Port_http)/j4p --product tomcat read "java.lang:type=Memory" HeapMemoryUsage max` / 1024 / 1024 ))
+    echo $(Color WHITE)$(Color BOLD)$(Color UNDERL)$HEAP_MEMORY$(Color) Mo
+  else
+    echo -e $(Color BOLD)$(Color YELLOW)No JMX$(Color)
+fi
+}
+
+
+
+Jvm_ram_memory() {
+RAM_MEMORY=$(( ` cut -d' ' -f2 <<<cat /proc/$(Pid)/statm 2>/dev/null || echo "0" ` / 1024 ))
+echo $RAM_MEMORY Mo
+}
+
+Jvm_cpu_use() {
+CPU_USE=$( ps -p $(Pid) -o %cpu 2>/dev/null | tail -n 1  )
+CPU_USE=${CPU_USE%.*}
+if [[ $CPU_USE -lt 10 ]]
+  then
+    echo $(Color BOLD)$(Color GREEN)$CPU_USE %$(Color)
+  else
+    echo $(Color BOLD)$(Color RED)$CPU_USE %$(Color)
+fi
+}
+
+Session_active_count() {
+if [[ $(Check_jmx4perl) == *OK* ]]
+  then
+    CONNECTION_COUNT=$(/usr/bin/jmx4perl http://localhost:$(Port_http)/j4p --product tomcat --method post read "Catalina:type=Manager,host=localhost,context=/gamma" activeSessions)
+    echo -e $(Color WHITE)$(Color BOLD)$(Color UNDERL)$CONNECTION_COUNT$(Color)
+  else  
+    echo -e $(Color BOLD)$(Color YELLOW)No JMX$(Color)
+fi
+}
+
+Session_max_active_count() {
+if [[ $(Check_jmx4perl) == *OK* ]]
+  then
+    CONNECTION_COUNT_MAX=$(/usr/bin/jmx4perl http://localhost:$(Port_http)/j4p --product tomcat --method post read "Catalina:type=Manager,host=localhost,context=/gamma" maxActive )
+    echo -e $(Color WHITE)$(Color BOLD)$(Color UNDERL)$CONNECTION_COUNT_MAX$(Color)
+  else
+    echo -e $(Color BOLD)$(Color YELLOW)No JMX$(Color)
+fi
+}
+
+Request_count() {
+if [[ $(Check_jmx4perl) == *OK* ]]
+  then
+    REQUEST_COUNT=$(/usr/bin/jmx4perl http://localhost:$(Port_http)/j4p --product tomcat --method post read Catalina:type=GlobalRequestProcessor,name=\"http-nio2-$(Port_http)\" requestCount)
+    REQUEST_COUNT_STAT=$REQUEST_COUNT
+    echo -e $(Color WHITE)$(Color BOLD)$(Color UNDERL)$REQUEST_COUNT$(Color)
+  else
+    echo -e $(Color BOLD)$(Color YELLOW)No JMX$(Color)
+fi
+}
+
+Avg_process_time() {
+
+Request_count >/dev/null
+if [[ $(Check_jmx4perl) == *OK* ]]
+  then
+    PROCESS_TIME=$(/usr/bin/jmx4perl http://localhost:$(Port_http)/j4p --product tomcat --method post read Catalina:type=GlobalRequestProcessor,name=\"http-nio2-$(Port_http)\" processingTime)
+    AVG_PROCESS_TIME=$(echo "$PROCESS_TIME / $REQUEST_COUNT_STAT" | bc )
+    #AVG_PROCESS_TIME=$REQUEST_COUNT_STAT
+    echo -e $(Color WHITE)$(Color BOLD)$(Color UNDERL)$AVG_PROCESS_TIME ms$(Color)
+  else
+    echo -e $(Color BOLD)$(Color YELLOW)No JMX$(Color)
+fi
+}
+
+Thread_pool_buzyness() {
+if [[ $(Check_jmx4perl) == *OK* ]]
+  then
+    CURR_THREAD_POOL_BUSY=$(/usr/bin/jmx4perl http://localhost:$(Port_http)/j4p --product tomcat --method post read Catalina:type=ThreadPool,name=\"http-nio2-$(Port_http)\" currentThreadsBusy)
+    THREAD_POOL_MAX=$(/usr/bin/jmx4perl http://localhost:$(Port_http)/j4p --product tomcat --method post read Catalina:type=ThreadPool,name=\"http-nio2-$(Port_http)\" maxThreads)
+    PERCENT_THREAD_POOL_BUSY=$(echo "(100 * $CURR_THREAD_POOL_BUSY ) / $THREAD_POOL_MAX" | bc)
+  if [ $PERCENT_THREAD_POOL_BUSY -lt 80 ] 
+    then 
+      echo -e $(Color GREEN)$(Color BOLD)$(Color UNDERL)$CURR_THREAD_POOL_BUSY / $THREAD_POOL_MAX$(Color)
+    else
+      echo -e $(Color RED)$(Color BOLD)$(Color UNDERL)$CURR_THREAD_POOL_BUSY / $THREAD_POOL_MAX$(Color)
+  fi
+  else
+    echo -e $(Color BOLD)$(Color YELLOW)No JMX$(Color)
+fi
+
+}
+
+Error_average() {
+if [[ $(Check_jmx4perl) == *OK* ]]
+  then
+    ERROR_COUNT=$(/usr/bin/jmx4perl http://localhost:$(Port_http)/j4p --product tomcat --method post read Catalina:type=GlobalRequestProcessor,name=\"http-nio2-$(Port_http)\" errorCount)
+    REQUEST_COUNT=$(/usr/bin/jmx4perl http://localhost:$(Port_http)/j4p --product tomcat --method post read Catalina:type=GlobalRequestProcessor,name=\"http-nio2-$(Port_http)\" requestCount)
+    PERCENT_ERROR_COUNT=$(echo "(100 * $ERROR_COUNT ) / $REQUEST_COUNT" | bc)
+## taux Erreur sous 2%
+  if [ $PERCENT_ERROR_COUNT -lt 2 ]
+    then
+      echo -e $(Color GREEN)$(Color BOLD)$(Color UNDERL)$PERCENT_ERROR_COUNT %$(Color)
+    else
+      echo -e $(Color RED)$(Color BOLD)$(Color UNDERL)$ERROR_COUNT/$REQUEST_COUNT$(Color)
+  fi
+  else
+    echo -e $(Color BOLD)$(Color YELLOW)No JMX$(Color)
+fi
+
+
+}
 
 Systemd_exist() {
 which systemd &>/dev/null
@@ -203,6 +373,30 @@ if [ -z $CATALINA_HOME ]
 fi
 }
 
+
+Jvm_xmx_config() {
+JVM_XMX_CONFIG=$(cat $CONFIG_PATH | grep -v "#"|egrep -o 'Xmx[0-9]*'|tr '\r' ' '| sed -e "s/Xmx//g")
+echo $JVM_XMX_CONFIG
+}
+
+Jvm_xms_config() {
+JVM_XMS_CONFIG=$(cat $CONFIG_PATH | grep -v "#"|egrep -o 'Xms[0-9]*'|tr '\r' ' '| sed -e "s/Xms//g")
+echo $JVM_XMS_CONFIG
+}
+
+
+Jvm_xmx() {
+JVM_XMX=$(ps -ef|grep $(Pid)| grep -v grep|egrep -o 'Xmx[0-9]*'|tr '\r' ' '| sed -e "s/Xmx//g")
+echo $JVM_XMX
+}
+
+Jvm_xms() {
+JVM_XMS=$(ps -ef|grep $(Pid)| grep -v grep|egrep -o 'Xms[0-9]*' |tr '\r' ' '| sed -e "s/Xms//g")
+echo $JVM_XMS
+}
+
+
+
 start() {
 $CATALINA_HOME/bin/startup.sh
 }
@@ -233,8 +427,22 @@ fi
 
 purge_logs() {
 echo Purge des logs
-rm -rf $CATALINA_BASE/logs/*
-rm -rf $CATALINA_BASE/webapps/lsprh/WEB-INF/logs/*
+if [ -z $CATALINA_BASE -o -z $LOG_DIR ]
+  then
+   echo "Une des variables nécessaires n'est pas definies" 
+    exit 1;
+  else
+   echo "Suppression et nombre de fichiers supprimes:"
+   for i in $CATALINA_BASE/logs $CATALINA_BASE/webapps/gamma/WEB-INF/logs $LOG_DIR
+   do 
+    printf "* $i: " 
+    printf $(find $i -type f -name "*.log" -print -delete | wc -l)
+    if [ ${PIPESTATUS[0]} -eq 0 ] 
+      then echo -e "$(Color GREEN)\tOK$(Color)"
+      else echo -e "$(Color RED)\tKO$(Color)" 
+    fi
+   done   
+fi
 }
 
 purge_cache() {
@@ -284,6 +492,8 @@ echo "#######################"
 Net_threads
 echo "#######################"
 Db_sessions
+echo "#######################"
+
 
 }
 #################
